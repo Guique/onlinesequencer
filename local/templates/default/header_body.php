cb2e9da33d4118b34d1ea410024e600b3cbbe51e
Jacob Morgan <jacobmorgan715@gmail.com>
2016-05-16 21:17:49 -0700

    Update header_body.php

--- local/templates/default/header_body.php
+++ local/templates/default/header_body.php
@@ -43,7 +43,7 @@ showNavLink('/experiments', 'Experiments');
             Welcome back <span id="member_username"><a href="/members/<?php echo $settings['uid']; ?>"><?php echo $settings['username']; ?></a></span>! <a id="member_logout" class="linkbutton" href="javascript:;">Log out</a>
         </div>
 
-        <div id="affiliate_link">Professional sequencing software: <a href="http://www.amazon.com/gp/product/B00CHZG1FE/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00CHZG1FE&linkCode=as2&tag=onlinseque-20">FL Studio</a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=onlinseque-20&l=as2&o=1&a=B00CHZG1FE" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /></div>
+        <div id="affiliate_link">Professional sequencing software: <a href="http://www.amazon.com/gp/product/B00CHZG1FE/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00CHZG1FE&linkCode=as2&tag=onlinseque-20">FL Studio</a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=onlinseque-20&l=as2&o=1&a=B00CHZG1FE" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /></div>
     </div>
     <span id="nav_right"><?php if(isset($nav_right)) echo $nav_right; ?></span>
 </div>
